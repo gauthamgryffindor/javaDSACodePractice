ef050a6f6b28596f4ec896cd44efd43650152b78
Gautham <80663073+gauthamgryffindor@users.noreply.github.com>
2025-03-03 07:05:27 +0530

    added the reverse number program

---
+++ Math/ReverseNumber.java
@@ -0,0 +1,20 @@
+package Math;
+import java.util.*;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int rev=0;
+        while(n>0){
+            int ld=n%10;
+            rev=(rev*10)+ld;
+            n=n/10;
+            
+        }
+
+        if(rev>0)
+            System.out.println(rev);
+        else
+         System.out.println(n);
+    }
+}
